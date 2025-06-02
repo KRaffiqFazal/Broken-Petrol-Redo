7efe096d3f69de5efd92578f7a83f897aaf8f8de
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2025-06-02 11:55:53 +0100

    Vehicle factory created

---
+++ Factories/VehicleFactory.cs
@@ -0,0 +1,33 @@
+﻿using Broken_Petrol_Redo.Classes;
+using Broken_Petrol_Redo.Interfaces;
+
+
+namespace Broken_Petrol_Redo.Factories;
+
+public class VehicleFactory
+{
+    public IVehicle CreateVehicle()
+    {
+        Random rnd = new Random();
+        
+        string[] vehicleTypes = { "Car", "Van", "HGV" };
+        string vehicleType = vehicleTypes[rnd.Next(0, vehicleTypes.Length)];
+        
+        string[] fuelTypes = { "Petrol", "Diesel",  "LPG" };
+        string fuelType;
+        
+        switch (vehicleType)
+        {
+            case "Car":
+                fuelType = fuelTypes[rnd.Next(0, fuelTypes.Length)];
+                return new Car(fuelType, rnd.Next(1, Car.MaxCapacity));
+            case "Van":
+                fuelType = fuelTypes[rnd.Next(1, fuelTypes.Length)];
+                return new Van(fuelType, rnd.Next(1, Van.MaxCapacity));
+            case "HGV":
+                return new Hgv("Diesel", rnd.Next(1, Hgv.MaxCapacity));
+            default:
+                throw new ApplicationException("Invalid vehicle type");
+        }
+    }
+}
