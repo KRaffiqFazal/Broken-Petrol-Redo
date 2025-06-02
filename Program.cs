0c22821ef1e516d54a8574bfd4bba1275c92d1d9
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2025-06-02 12:05:48 +0100

    tested factory functionality.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Broken_Petrol_Redo;
+﻿using Broken_Petrol_Redo.Factories;
+using Broken_Petrol_Redo.Interfaces;
+
+namespace Broken_Petrol_Redo;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        VehicleFactory factory = new VehicleFactory();
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(factory.CreateVehicle().GetType());
+        }
+
+        IVehicle vehicle = factory.CreateVehicle();
+        Console.WriteLine(vehicle.GetType());
     }
 }
