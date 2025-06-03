d9c7574d68c8c18b33fd6061317fdd8fc16d8413
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2025-06-03 14:00:46 +0100

    Tested worker to enqueue vehicles.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,25 @@
 ﻿using Broken_Petrol_Redo.Factories;
 using Broken_Petrol_Redo.Interfaces;
-
+using Broken_Petrol_Redo.Workers;
 namespace Broken_Petrol_Redo;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        VehicleFactory factory = new VehicleFactory();
-        for (int i = 0; i < 10; i++)
+        EnqueueWorker worker = new EnqueueWorker();
+        Task.Run(() => worker.StartQueueing());
+        IVehicle currentVehicle = null;
+        for (int i = 0; i < 300000; i++)
         {
-            Console.WriteLine(factory.CreateVehicle().GetType());
+            currentVehicle = worker.Dequeue();
+            if (currentVehicle != null)
+            {
+                Console.WriteLine(currentVehicle.GetType());
+            }
         }
-
-        IVehicle vehicle = factory.CreateVehicle();
-        Console.WriteLine(vehicle.GetType());
+        worker.StopQueueing();
+        Console.WriteLine("STOPPED");
+        Thread.Sleep(3000);
     }
 }
