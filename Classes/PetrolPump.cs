cf788ad2564305f76b73d679c0699456431adbd6
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2025-06-10 18:25:56 +0100

    wip fuel pumps

---
+++ Classes/PetrolPump.cs
@@ -0,0 +1,31 @@
+﻿using Broken_Petrol_Redo.Interfaces;
+
+namespace Broken_Petrol_Redo.Classes;
+
+public class PetrolPump : IFuelPump
+{
+    public IFunctioningVehicle FuellingVehicle { get; set; }
+    public Dictionary<string, int> LitresDispensed { get; set; }
+    public bool CanFuel { get; set; }
+    public bool Blocked { get; set; }
+    
+    public void ReleaseVehicle()
+    {
+        LitresDispensed[FuellingVehicle.CurrentVehicle.FuelType] += FuellingVehicle.CurrentVehicle.Fueled();
+        FuellingVehicle = null;
+        CanFuel = true;
+    }
+
+    public PetrolPump()
+    {
+        FuellingVehicle = null;
+        LitresDispensed = new(){
+            { "Petrol", 0},
+            { "Diesel", 0},
+            { "LPG", 0 }
+        };
+        CanFuel = true;
+        Blocked = false;
+    }
+
+}
---
+++ Interfaces/IFuelPump.cs
@@ -0,0 +1,11 @@
+﻿namespace Broken_Petrol_Redo.Interfaces;
+
+public interface IFuelPump
+{
+    IFunctioningVehicle FuellingVehicle { get; set; }
+    Dictionary<string, int> LitresDispensed { get; set; }
+    bool CanFuel { get; set; }
+    bool Blocked { get; set; }
+    void ReleaseVehicle();
+    
+}
