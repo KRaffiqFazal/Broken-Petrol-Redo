fb6f2652a792ec9ac6f966c95125cc9d06ac68a2
Kazim Raffiq-Fazal <115145581+KRaffiqFazal@users.noreply.github.com>
2025-06-12 11:16:57 +0100

    Added petrol pump paths and method to start fuelling new vehicles and remove fueled vehicles.

--- Workers/EnqueueWorker.cs
+++ Workers/EnqueueWorker.cs
@@ -1,4 +1,5 @@
 ﻿using System.Runtime.InteropServices;
+using Broken_Petrol_Redo.Classes;
 using Broken_Petrol_Redo.Factories;
 using Broken_Petrol_Redo.Interfaces;
 
@@ -7,6 +8,17 @@ namespace Broken_Petrol_Redo.Workers;
 public class EnqueueWorker
 {
     private List<IFunctioningVehicle> _queue = new();
+    private IFuelPump[][] _petrolPumps = {
+        [
+            new PetrolPump(), new PetrolPump(), new PetrolPump()
+        ],
+        [
+            new PetrolPump(), new PetrolPump(), new PetrolPump()
+        ],
+        [
+            new PetrolPump(), new PetrolPump(), new PetrolPump()
+        ]
+    };
     private readonly CancellationTokenSource _cancellationTokenSource = new();
 
     private void Enqueue(IFunctioningVehicle waitingVehicle)
@@ -18,7 +30,7 @@ public class EnqueueWorker
         _queue.Add(waitingVehicle);
     }
 
-    public IFunctioningVehicle Dequeue()
+    private IFunctioningVehicle Dequeue()
     {
         try
         {
@@ -67,7 +79,65 @@ public class EnqueueWorker
         }
     }
 
-    public void StopQueueing()
+    public async Task RemoveFuelledVehicles()
+    {
+        CancellationToken  ct = _cancellationTokenSource.Token;
+        while (!ct.IsCancellationRequested)
+        {
+            try
+            {
+                foreach (var path in _petrolPumps)
+                {
+                    foreach (var pump in path)
+                    {
+                        if (pump.FuellingVehicle.IsCompleted)
+                        {
+                            pump.ReleaseVehicle();
+                        }
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+        }
+    }
+
+    public async Task FuelVehicles()
+    {
+        CancellationToken  ct = _cancellationTokenSource.Token;
+        while (!ct.IsCancellationRequested)
+        {
+            try
+            {
+                KeyValuePair<int,int> availablePump = GetAvailablePumpIndex();
+                IFunctioningVehicle toFuel = Dequeue();
+                _petrolPumps[availablePump.Key][availablePump.Value].AttachVehicle(toFuel);
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+        }
+    }
+
+    private KeyValuePair<int, int> GetAvailablePumpIndex()
+    {
+        for (int i=0; i<_petrolPumps.Length; i++)
+        {
+            for (int j=0; j<_petrolPumps[i].Length; j++)
+            {
+                if (_petrolPumps[i][j] is { Blocked: false, CanFuel: true })
+                {
+                    return new KeyValuePair<int, int>(i, j);
+                }
+            }
+        }
+        return  new KeyValuePair<int, int>(-1, -1);
+    }
+
+    public void StopFunction()
     {
         _cancellationTokenSource.Cancel();
     }
